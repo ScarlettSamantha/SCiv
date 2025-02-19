adced78fea1535d193c30277df9a1800f8245031
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-02-19 13:10:20 +0100

    ✨  Implement load_tiles method to dynamically load tile classes and enhance tile generation logic
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- system/generators/random.py
+++ system/generators/random.py
@@ -1,6 +1,11 @@
+from numpy import isin
+from torch import rand
+from os.path import join, dirname
 from data.tiles.tile import Tile
 from system.generators.base import BaseGenerator
 from panda3d.core import BitMask32
+from system.pyload import PyLoad
+import random
 
 
 class RandomGenerator(BaseGenerator):
@@ -10,6 +15,14 @@ class RandomGenerator(BaseGenerator):
         self.grid = self.config.grid  # Syntax Sugar
         super().__init__(config)
 
+    def load_tiles(self):
+        from data.tiles.tile import Tile
+
+        classes = PyLoad.load_classes("data/tiles", base_classes=Tile)
+        if "Tile" in classes:
+            del classes["Tile"]
+        return classes
+
     def generate(self):
         """Set up a grid of hexagon tiles with geometry-based collisions."""
         # Load and adjust the hex model.
@@ -18,6 +31,7 @@ class RandomGenerator(BaseGenerator):
         # Rotate the model so it lies flat.
         hex_model.setHpr(180, 90, 90)
 
+        tiles = self.load_tiles()
         for col in range(self.config.cols):
             for row in range(self.config.rows):
                 x = col * self.config.col_spacing
@@ -36,7 +50,17 @@ class RandomGenerator(BaseGenerator):
                 # Optionally, tag the tile for identification
                 new_hex.setTag("tile_id", tag)
 
-                obj_instance = Tile(tag, col, row, new_hex)
+                # Remove the base tile model from the list of tiles
+
+                tile = random.choice(list(tiles.values()))
+
+                obj_instance: Tile = tile(tag, col, row, new_hex)
+                color = obj_instance.color()
+                if isinstance(color, tuple) and len(color) == 3:
+                    color = color + (float(1),)
+                texture_path = join(dirname(__file__), "../../", obj_instance.texture())
+                texture = self.config.base.loader.loadTexture(texture_path)
+                obj_instance.node.setTexture(texture)
 
                 self.map[tag] = obj_instance
                 self.grid[(col, row)] = obj_instance
