c1289cf361d8f3681b479d0d559bdb3575cf039a
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-02-19 05:55:15 +0100

    ✨ Refactor Input class to improve activation logic and enhance object picking feedback
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- managers/input.py
+++ managers/input.py
@@ -3,38 +3,38 @@ from panda3d.core import (
     CollisionNode,
     CollisionTraverser,
     CollisionHandlerQueue,
-    BitMask32
+    BitMask32,
 )
 from typing import Any
 from mixins.singleton import Singleton
 from direct.showbase.MessengerGlobal import messenger
 
+
 class Input(Singleton):
-    
-    def __init__(self):
-        self.base: Any = None
-        self.active: bool = True
-        
+    def __init__(self, base):
+        self.base: Any = base
+        self.active: bool = False
+
     def de_activate(self):
         self.active = False
-        
+
     def activate(self):
         self.active = True
-        
+
     def __setup__(self, base, *args: Any, **kwargs: Any) -> None:
         from managers.world import World
-        
+
         self.base = base
         self.map = World.get_instance(self.base)
-        
+
         return super().__setup__(*args, **kwargs)
-        
+
     def inject_into_camera(self):
         self.picker = CollisionTraverser()
         self.pq = CollisionHandlerQueue()
 
         # 2) Create a collision node containing a ray solid
-        picker_node = CollisionNode('mouseRay')
+        picker_node = CollisionNode("mouseRay")
         self.pickerRay = CollisionRay()
         picker_node.addSolid(self.pickerRay)
 
@@ -47,12 +47,15 @@ class Input(Singleton):
         # 4) Add the collider to the traverser
         self.picker.addCollider(self.pickerNP, self.pq)
         self.register()
-    
+
     def pick_object(self):
         """
         Cast a ray from the camera using the current mouse position
         and return (print) whichever object is hit first (closest).
         """
+        if not self.active:
+            return  # Input is disabled
+
         if not self.base.mouseWatcherNode.hasMouse():
             print("No mouse in window, cannot pick.")
             return
@@ -72,13 +75,14 @@ class Input(Singleton):
             picked_obj = entry.getIntoNodePath()
             # If you stored a tag (like tile_id), you can retrieve it:
             tile_id = picked_obj.getNetTag("tile_id")
-            messenger.send("tile-clicked", [tile_id])
+            messenger.send("system.input.user.tile_clicked", [tile_id])
 
             return picked_obj
         else:
-            print("No object picked.")
+            print(
+                "No object picked. This might the be margin between tiles or outside the game field."
+            )  # This is probibly the margin between the tiles or between the tiles and the camera
             return None
-        
+
     def register(self):
-        # Example key to test picking (when 'p' is pressed)
         self.base.accept("mouse1", self.pick_object)
