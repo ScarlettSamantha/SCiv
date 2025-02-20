5e2e227bd860787915af291ab02d982663e6fe2c
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-02-20 20:15:09 +0100

    ✨ Update Improvements class to use Optional for improvements list and enhance type annotations
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- gameplay/improvements.py
+++ gameplay/improvements.py
@@ -1,17 +1,20 @@
-from __future__ import annotations
-from typing import List
+from typing import List, Optional
+from gameplay.improvement import Improvement
 
 
 class Improvements:
-    def __init__(self, improvements: List = None):
+    def __init__(self, improvements: Optional[List] = None):
         self._improvements = {}
         if improvements is not None:
             for item in improvements:
                 self.add(item)
 
-    def add(self, value: "improvement"):  # noqa F821
+    def add(self, value: Improvement):
         self._improvements[len(self._improvements.keys())] = value
 
-    def __add__(self, value: "improvement"):  # noqa F821
+    def __add__(self, value: Improvement):
         self.add(value)
         return self
+
+    def get_all(self) -> List[Improvement]:
+        return list(self._improvements.values())
