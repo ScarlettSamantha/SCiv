81a59c00e9b2b18afb0a0cf90eede548dab9cf89
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-02-20 06:18:43 +0100

    ✨ Initialize UI and World instances in BaseMenu for improved menu functionality
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- menus/_base.py
+++ menus/_base.py
@@ -1,5 +1,11 @@
 from direct.showbase.ShowBase import ShowBase
 
+
 class BaseMenu(ShowBase):
     def __init__(self):
+        from managers.ui import ui
+        from managers.world import World
+
         self.frame = None
+        self.ui = ui.get_instance()
+        self.world = World.get_instance()
