12536336f29b8de08796b88cfc488c329d4014bd
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-02-20 09:20:36 +0100

    ✨ Implement Units class with methods for adding and removing units
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- gameplay/_units.py
+++ gameplay/_units.py
@@ -1,3 +1,15 @@
 from __future__ import annotations
+from typing import List
+
+from gameplay.units.classes._base import UnitBaseClass
+
+
 class Units:
-    pass
+    def __init__(self):
+        self.units: List[UnitBaseClass] = []
+
+    def add_unit(self, unit: UnitBaseClass):
+        self.units.append(unit)
+
+    def remove_unit(self, unit: UnitBaseClass):
+        self.units.remove(unit)
