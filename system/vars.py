5e5767f429e5647bc7c7ba326d4c5043c522f7be
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-02-27 12:23:16 +0100

    ✨ Add versioning and meta information for the application
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

---
+++ system/vars.py
@@ -0,0 +1,17 @@
+from typing import List, Tuple
+
+VERSION_MAJOR: int = 0
+VERSION_MINOR: int = 1
+VERSION_PATCH: int = 0
+VERSION: Tuple[int, int, int] = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
+VERSION_NAME: str = "Alpha"
+VERSION_STRING: str = ".".join(str(i) for i in VERSION)
+VERSION_NAME_STRING: str = f"{VERSION_NAME}-{VERSION_STRING}"
+
+# Meta information
+APPLICATION_TYPE: str = "Game"
+APPLICATION_NAME: str = "SCIV"
+
+CREATOR: Tuple[str, str] = ("Scarlett Samantha Verheul", "scarlett.verheul@gmail.com")
+
+AUTHORS: List[Tuple[str, str]] = [CREATOR]
