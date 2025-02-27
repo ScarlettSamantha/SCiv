7c891d176688abbebd707825463e1c0ffeaac24e
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-02-27 16:42:11 +0100

    ✨ Update version name string format for clarity
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- system/vars.py
+++ system/vars.py
@@ -6,7 +6,7 @@ VERSION_PATCH: int = 0
 VERSION: Tuple[int, int, int] = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 VERSION_NAME: str = "Alpha"
 VERSION_STRING: str = ".".join(str(i) for i in VERSION)
-VERSION_NAME_STRING: str = f"{VERSION_NAME}-{VERSION_STRING}"
+VERSION_NAME_STRING: str = f"{VERSION_STRING}-{VERSION_NAME}"
 
 # Meta information
 APPLICATION_TYPE: str = "Game"
