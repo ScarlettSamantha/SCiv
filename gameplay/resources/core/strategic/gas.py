d75ea7f77d1600ca7e85cd352dec647703b95a8a
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-01 08:34:09 +0100

    ✨ Update Gas and Oil resources to include terrain-specific spawn chances and introduce spawn_type and test if spawn_type both works properly.
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- gameplay/resources/core/strategic/gas.py
+++ gameplay/resources/core/strategic/gas.py
@@ -1,5 +1,8 @@
 from typing import Dict, Tuple, Type
 from data.terrain._base_terrain import BaseTerrain
+from data.terrain.coast import Coast
+from data.terrain.sea import Sea
+from gameplay.resource import ResourceSpawnablePlace
 from gameplay.resources.core.strategic.strategic_resource import BaseStrategyResource
 from managers.i18n import T_TranslationOrStr, _t
 
@@ -8,7 +11,8 @@ class Gas(BaseStrategyResource):
     key: str = "resource.core.strategic.gas"
     name: T_TranslationOrStr = _t("content.resources.core.gas.name")
     description: T_TranslationOrStr = _t("content.resources.core.gas.description")
-    spawn_chance: float | Dict[Type[BaseTerrain], float] = 15.0
+    spawn_chance: float | Dict[Type[BaseTerrain], float] = {Sea: 15.0, Coast: 5.0, BaseTerrain: 7.0}
+    spawn_type: ResourceSpawnablePlace = ResourceSpawnablePlace.BOTH
     spawn_amount: float | Tuple[float, float] = 3.0
 
     def __init__(self, value: int = 0):
--- gameplay/resources/core/strategic/oil.py
+++ gameplay/resources/core/strategic/oil.py
@@ -1,5 +1,8 @@
 from typing import Dict, Tuple, Type
 from data.terrain._base_terrain import BaseTerrain
+from data.terrain.coast import Coast
+from data.terrain.sea import Sea
+from gameplay.resource import ResourceSpawnablePlace
 from gameplay.resources.core.strategic.strategic_resource import BaseStrategyResource
 from managers.i18n import T_TranslationOrStr, _t
 
@@ -8,7 +11,8 @@ class Oil(BaseStrategyResource):
     key: str = "resource.core.strategic.oil"
     name: T_TranslationOrStr = _t("content.resources.core.oil.name")
     description: T_TranslationOrStr = _t("content.resources.core.oil.description")
-    spawn_chance: float | Dict[Type[BaseTerrain], float] = 15.0
+    spawn_chance: float | Dict[Type[BaseTerrain], float] = {Sea: 15.0, Coast: 5.0, BaseTerrain: 7.0}
+    spawn_type: ResourceSpawnablePlace = ResourceSpawnablePlace.BOTH
     spawn_amount: float | Tuple[float, float] = 3.0
 
     def __init__(self, value: int = 0):
