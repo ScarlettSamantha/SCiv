c1be3ae8984e124f403a9087bf031fd62d20a9be
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-01 08:35:24 +0100

    ✨ Add Lobster resource with terrain-specific spawn chances and properties
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

---
+++ gameplay/resources/core/bonus/lobster.py
@@ -0,0 +1,22 @@
+from typing import Dict, Tuple, Type
+from gameplay.resources.core.bonus.bonus_resource import BaseBonusResource
+from gameplay.resource import ResourceSpawnablePlace
+from data.terrain.coast import Coast
+from data.terrain.sea import Sea
+from data.terrain._base_terrain import BaseTerrain
+from managers.i18n import T_TranslationOrStr, _t
+
+
+class Lobster(BaseBonusResource):
+    key: str = "resource.core.bonus.clams"
+    name: T_TranslationOrStr = _t("content.resources.core.lobster.name")
+    description: T_TranslationOrStr = _t("content.resources.core.lobster.description")
+    spawn_chance: float | Dict[Type[BaseTerrain], float] = {
+        Sea: 5.0,
+        Coast: 7.0,
+    }
+    spawn_amount: float | Tuple[float, float] = 3.0
+    spawn_type: ResourceSpawnablePlace = ResourceSpawnablePlace.WATER
+
+    def __init__(self, value: int = 0):
+        super().__init__(value=value)
