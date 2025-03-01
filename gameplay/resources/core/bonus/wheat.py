1804c4afa01190264586981730b67dc191986af4
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-01 08:37:47 +0100

    ✨ Update Wheat resource to include terrain-specific spawn chances for FlatGrass and HillsGrass
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- gameplay/resources/core/bonus/wheat.py
+++ gameplay/resources/core/bonus/wheat.py
@@ -1,5 +1,7 @@
 from typing import Dict, Tuple, Type
 from data.terrain._base_terrain import BaseTerrain
+from data.terrain.flat_grass import FlatGrass
+from data.terrain.hills_grass import HillsGrass
 from gameplay.resources.core.bonus.bonus_resource import BaseBonusResource
 from managers.i18n import T_TranslationOrStr, _t
 
@@ -8,7 +10,7 @@ class Wheat(BaseBonusResource):
     key: str = "resource.core.bonus.wheat"
     name: T_TranslationOrStr = _t("content.resources.core.wheat.name")
     description: T_TranslationOrStr = _t("content.resources.core.wheat.description")
-    spawn_chance: float | Dict[Type[BaseTerrain], float] = 8.0
+    spawn_chance: float | Dict[Type[BaseTerrain], float] = {FlatGrass: 10, HillsGrass: 5, BaseTerrain: 1}
     spawn_amount: float | Tuple[float, float] = 5.0
 
     def __init__(self, value: int = 0):
