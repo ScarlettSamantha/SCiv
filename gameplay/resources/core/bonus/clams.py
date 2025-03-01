9842bcafc66802b712050c2bd6014b5c6800c15c
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-01 08:33:37 +0100

    ✨ Add Clams resource because to little sea difference with terrain-specific spawn chances and properties
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

---
+++ gameplay/resources/core/bonus/clams.py
@@ -0,0 +1,22 @@
+from typing import Dict, Tuple, Type
+from gameplay.resources.core.bonus.bonus_resource import BaseBonusResource
+from gameplay.resource import ResourceSpawnablePlace
+from data.terrain.coast import Coast
+from data.terrain.sea import Sea
+from data.terrain._base_terrain import BaseTerrain
+from managers.i18n import T_TranslationOrStr, _t
+
+
+class Clams(BaseBonusResource):
+    key: str = "resource.core.bonus.clams"
+    name: T_TranslationOrStr = _t("content.resources.core.clams.name")
+    description: T_TranslationOrStr = _t("content.resources.core.clams.description")
+    spawn_chance: float | Dict[Type[BaseTerrain], float] = {
+        Sea: 10.0,
+        Coast: 15.0,
+    }
+    spawn_amount: float | Tuple[float, float] = 3.0
+    spawn_type: ResourceSpawnablePlace = ResourceSpawnablePlace.WATER
+
+    def __init__(self, value: int = 0):
+        super().__init__(value=value)
