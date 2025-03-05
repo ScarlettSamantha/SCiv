3b4f2e0c04d3f2d27a88194909b6a383ab748b49
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-05 08:38:39 +0100

    ✨ Add on_press callback support to ButtonValue class
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- menus/kivy/elements/button_value.py
+++ menus/kivy/elements/button_value.py
@@ -1,14 +1,23 @@
-from typing import Any, Optional
+from typing import Any, Callable, Optional, Self
 from kivy.uix.button import Button
 
 
 class ButtonValue(Button):
-    def __init__(self, value: Optional[Any] = None, **kwargs):
+    def __init__(self, value: Optional[Any] = None, on_press: Optional[Callable] = None, **kwargs):
         super().__init__(**kwargs)
         self.value: Optional[Any] = value
+        self._on_press: Optional[Callable[[Self]]] = on_press
 
     def set_value(self, value):
         self.value = value
 
     def get_value(self) -> Optional[Any]:
         return self.value
+
+    def action_dispatch(self):
+        if self._on_press is not None:
+            self._on_press(self)
+
+    def on_press(self):
+        if self._on_press is not None:
+            self.action_dispatch()
