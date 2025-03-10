701a684a8ae8c8ed34d1bd114c99f8bf824ebc8a
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-10 07:46:50 +0100

    ✨ Extend BaseEntity to inherit from DirectObject and integrate showbase Cached instance
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- system/entity.py
+++ system/entity.py
@@ -1,12 +1,18 @@
-from typing import Optional
 from abc import ABC
+from typing import Optional
+
+from direct.showbase.DirectObject import DirectObject
+
+from helpers.cache import Cache
 
 
-class BaseEntity(ABC):
+class BaseEntity(ABC, DirectObject):
     def __init__(self):
+        super().__init__()
         self.entity_key: Optional[str] = None
         self.entity_type_ref: Optional[str] = None
         self.is_registered: bool = False
+        self.base = Cache._instance
 
     def get_registered_status(self) -> bool:
         return self.is_registered
