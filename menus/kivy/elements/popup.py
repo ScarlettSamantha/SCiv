327de2daa706f2422f4b79bd5ee7ef179966f974
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-10 08:40:32 +0100

    ♻️ 🐛 Fixed Refactor clicking trough the buttons, all elements are not properties instead of locally scoped.
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- menus/kivy/elements/popup.py
+++ menus/kivy/elements/popup.py
@@ -6,10 +6,12 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 
+from helpers.cache import Cache
 from managers.i18n import T_TranslationOrStr, T_TranslationOrStrOrNone
+from menus.kivy.mixins.collidable import CollisionPreventionMixin
 
 
-class ModalPopup(Popup):
+class ModalPopup(Popup, CollisionPreventionMixin):
     def __init__(
         self,
         title: T_TranslationOrStr,
@@ -31,39 +33,51 @@ class ModalPopup(Popup):
         if isinstance(message, T_TranslationOrStr | T_TranslationOrStrOrNone):
             message = str(message)
 
-        super().__init__(title=title, size=(width, height), size_hint=(0.30, 0.25), **kwargs)
+        self.base = Cache.get_showbase_instance()
+
+        super().__init__(title=title, base=self.base, size=(width, height), size_hint=(0.30, 0.25), **kwargs)
 
         self._message: str = message
         self._confirm_callback: Optional[Callable] = confirm_callback
         self._cancel_callback: Optional[Callable] = cancel_callback
         self.auto_dismiss = False
 
-        # Build a simple layout.
-        layout = BoxLayout(orientation="vertical", padding=10, spacing=10)
+        self.confirm_button: Optional[Button] = None
+        self.close_button: Optional[Button] = None
 
-        # Show the message
-        layout.add_widget(Label(text=self._message))
+        self.accept_button: Optional[Button] = None
+        self.decline_button: Optional[Button] = None
+
+        self.button_layout: Optional[BoxLayout] = None
+        self.layout: BoxLayout = BoxLayout(orientation="vertical", padding=10, spacing=10)
+
+        self.bind(on_open=self.on_open)
+        self.bind(on_dismiss=self.on_close)
+
+        self.layout.add_widget(Label(text=self._message))
 
         # Build button row
-        btn_layout = BoxLayout(orientation="horizontal", size_hint=(1, 0.3), spacing=10)
+        self.button_layout = BoxLayout(orientation="horizontal", size_hint=(1, 0.3), spacing=10)
         if self._confirm_callback and self._cancel_callback:
-            accept_btn = Button(text="Accept")
-            decline_btn = Button(text="Decline")
-            accept_btn.bind(on_release=lambda _: self._on_confirm())
-            decline_btn.bind(on_release=lambda _: self._on_cancel())
-            btn_layout.add_widget(accept_btn)
-            btn_layout.add_widget(decline_btn)
+            self.accept_button = Button(text="Accept")
+            self.decline_button = Button(text="Decline")
+            self.accept_button.bind(on_release=lambda _: self._on_confirm())
+            self.decline_button.bind(on_release=lambda _: self._on_cancel())
+            self.button_layout.add_widget(self.accept_button)
+            self.button_layout.add_widget(self.decline_button)
         elif self._confirm_callback:
-            confirm_btn = Button(text="Confirm")
-            confirm_btn.bind(on_release=lambda _: self._on_confirm())
-            btn_layout.add_widget(confirm_btn)
+            self.confirm_btn = Button(text="Confirm")
+            self.confirm_btn.bind(on_release=lambda _: self._on_confirm())
+            self.confirm_btn.disabled = True
+            self.button_layout.add_widget(self.confirm_btn)
         else:
-            close_btn = Button(text="Close")
-            close_btn.bind(on_release=lambda _: self.dismiss())
-            btn_layout.add_widget(close_btn)
+            self.close_btn = Button(text="Close")
+            self.close_btn.bind(on_release=lambda _: self.dismiss())
+            self.close_btn.disabled = True
+            self.button_layout.add_widget(self.close_btn)
 
-        layout.add_widget(btn_layout)
-        self.content = layout  # A Popup must have only one widget as content
+        self.layout.add_widget(self.button_layout)
+        self.content = self.layout  # A Popup must have only one widget as content
 
     def _on_confirm(self) -> None:
         if self._confirm_callback:
@@ -74,6 +88,18 @@ class ModalPopup(Popup):
         if self._cancel_callback:
             self._cancel_callback()
         self.dismiss()
+
+    def on_close(self, *args, **kwargs):
+        if self.close_btn is not None:
+            self.close_btn.disabled = True
+
+        self.unregister_non_collidable(self.layout)
+
+    def on_open(self, *args, **kwargs):
+        if self.close_btn is not None:
+            self.close_btn.disabled = False
+
+        self.register_non_collidable(self.layout)
 
 
 # Non-modal popup: simply a ModalPopup with modal behavior turned off.
@@ -87,33 +113,6 @@ class NonModalPopup(ModalPopup):
 class PopupDraggableMixin:
     dragging: bool = False
     _touch_offset = (0, 0)
-
-    def on_touch_down(self, touch):
-        # Add a debug print to see if this method is called
-        print("[DEBUG] on_touch_down, collide=", self.collide_point(*touch.pos), "pos=", self.pos, "touch=", touch.pos)
-        if self.collide_point(*touch.pos):
-            self.dragging = True
-            self._touch_offset = (self.x - touch.x, self.y - touch.y)
-            return True
-        return super().on_touch_down(touch)
-
-    def on_touch_move(self, touch):
-        if self.dragging:
-            # Another debug print to confirm movement
-            print(
-                "[DEBUG] on_touch_move, new popup pos=",
-                (touch.x + self._touch_offset[0], touch.y + self._touch_offset[1]),
-            )
-            self.pos = (touch.x + self._touch_offset[0], touch.y + self._touch_offset[1])
-            return True
-        return super().on_touch_move(touch)
-
-    def on_touch_up(self, touch):
-        if self.dragging:
-            print("[DEBUG] on_touch_up, stopping drag")
-            self.dragging = False
-            return True
-        return super().on_touch_up(touch)
 
 
 # DraggableModalPopup: The popup itself is draggable.
