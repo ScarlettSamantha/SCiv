cb3d67f1926a5470728bf8701979e52e06f50e98
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-11 07:42:46 +0100

    🗑️ Remove unused Conditions class from gameplay module
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- gameplay/conditions.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-from system.saving import SaveAble
-
-
-class Conditions(SaveAble):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
