963eb2f7dd4d7e703a97c2bd5c08b6325474a5a7
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-11 08:59:44 +0100

    ✏️ Fix typo in sejong's name
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

---
+++ gameplay/leaders/sejong.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from gameplay.effect import Effects
+from gameplay.leader import Leader
+from managers.i18n import t_
+
+
+class Sejong(Leader):
+    def __init__(self) -> None:
+        super().__init__(
+            key="core.leaders.sejong",
+            name=t_("civilization.korea.leaders.sejong.name"),
+            description=t_("civilization.korea.leaders.sejong.description"),
+            icon="civilization/korea/leaders/sejong/leader_icon.png",
+        )
+        self._effects = Effects()
