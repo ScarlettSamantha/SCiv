56e41b74331b7eb850e1acef103c9279dbf8d80f
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-12 17:20:30 +0100

    ♻️ Refactor imports in item and builder modules
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- gameplay/units/core/classes/civilian/builder.py
+++ gameplay/units/core/classes/civilian/builder.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
-from typing import Any
-from managers.i18n import t_
 
-from gameplay.units.core.classes.civilian._base import CoreCivilianBaseClass
+from typing import Any
+
 from gameplay.promotion import Promotion, PromotionTree
-from openciv.engine.requires import RequiresPromotionComplete, RequiresPromotionTreeUnlocked
+from gameplay.units.core.classes.civilian._base import CoreCivilianBaseClass
+from managers.i18n import t_
+from system.requires import RequiresPromotionComplete, RequiresPromotionTreeUnlocked
 
 
 class BuilderPromotion(Promotion):
@@ -119,12 +120,14 @@ class BuilderPromotionTree(PromotionTree):
 
 
 class Builder(CoreCivilianBaseClass):
+    _model = "assets/models/units/pessent.glb"
+    buildable = True
+    key = "core.unit.class.builder"
+    name = t_("content.units.core.units.civilian.builder.name")
+    description = t_("content.units.core.units.civilian.builder.description")
+
     def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(
-            key="core.unit.class.builder",
-            name=t_("content.units.classes.core.builder.name"),
-            description=t_("content.units.classes.core.builder.description"),
-            icon=None,
             *args,
             **kwargs,
         )
