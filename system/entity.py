1e6a21caa384a32ce383db7a1d7f02f0b7a7321f
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-12 17:21:47 +0100

    🐛 Ensure Cache instance is set before accessing it in BaseEntity
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- system/entity.py
+++ system/entity.py
@@ -2,8 +2,12 @@ from abc import ABC
 from typing import Optional
 
 from direct.showbase.DirectObject import DirectObject
+from git import TYPE_CHECKING
 
 from helpers.cache import Cache
+
+if TYPE_CHECKING:
+    from main import Openciv
 
 
 class BaseEntity(ABC, DirectObject):
@@ -12,7 +16,11 @@ class BaseEntity(ABC, DirectObject):
         self.entity_key: Optional[str] = None
         self.entity_type_ref: Optional[str] = None
         self.is_registered: bool = False
-        self.base = Cache._instance
+
+        if Cache._instance is None:
+            raise AssertionError("Cache instance is not set.")
+
+        self.base: "Openciv" = Cache._instance
 
     def get_registered_status(self) -> bool:
         return self.is_registered
