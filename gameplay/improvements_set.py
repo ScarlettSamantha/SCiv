e19383263ff9df7a9de1098353a9b003a719054d
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-15 12:29:26 +0100

    ♻️  Refactor ImprovementsSet to track number of improvements and add remove method
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- gameplay/improvements_set.py
+++ gameplay/improvements_set.py
@@ -5,7 +5,8 @@ from gameplay.improvement import Improvement
 
 class ImprovementsSet:
     def __init__(self, improvements: Optional[List] = None):
-        self._improvements = []
+        self._improvements: List[Improvement] = []
+        self._num_improvements: int = 0
 
         if improvements is not None:
             for item in improvements:
@@ -13,6 +14,7 @@ class ImprovementsSet:
 
     def add(self, value: Improvement):
         self._improvements.append(value)
+        self._num_improvements += 1
 
     def __add__(self, value: Improvement):
         self.add(value)
@@ -21,7 +23,18 @@ class ImprovementsSet:
     def get_all(self) -> List[Improvement]:
         return self._improvements
 
+    def remove(self, value: Improvement):
+        self._improvements.remove(value)
+        self._num_improvements -= 1
+
     def __contains__(self, a) -> bool:
         if isinstance(a, type):
             return any(isinstance(i, a) for i in self._improvements)
         return a in self._improvements
+
+    def __len__(self) -> int:
+        return self._num_improvements
+
+    def on_turn_end(self, turn: int):
+        for item in self._improvements:
+            item.on_turn_end(turn)
