72cf166a6302f1af64604fb890e8155e7bc16bdd
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-20 10:16:18 +0100

    ♻️ Refactor ActionBar to inherit from DirectObject and update layout to GridLayout
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- menus/kivy/parts/action_bar.py
+++ menus/kivy/parts/action_bar.py
@@ -1,31 +1,33 @@
 from typing import Optional
 
+from direct.showbase.DirectObject import DirectObject
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 
 
-class ActionBar(BoxLayout):
-    def __init__(self, base, background_color=(0, 0, 0, 0), border=(0, 0, 0, 0), **kwargs):
-        self.background_color = background_color
-        self.border = border
-        self.background_image = None
-        super().__init__(**kwargs)
-        self.base = base
-        self.frame: Optional[BoxLayout] = None
+class ActionBar(BoxLayout, DirectObject):
+    def __init__(self, *args, **kwargs):
+        self.background_color = (0, 0, 0, 1)
+        self.border = (1, 1, 1, 1)
+        self.background_image = ""
+        super().__init__(*args, **kwargs)  # type: ignore # This is a false positive
+        self.frame: Optional[GridLayout] = None
 
-    def build(self) -> BoxLayout:
-        # --- Action Bar (Bottom Centered) ---
-        self.frame = BoxLayout(
-            orientation="horizontal",
+    def build(self) -> GridLayout:
+        self.frame = GridLayout(  # noqa: F821
+            orientation="lr-tb",
             size_hint=(None, None),
             width=1000,
             height=80,
             spacing=10,
             pos_hint={"center_x": 0.5, "y": 0},
+            cols=12,
+            rows=1,
         )
         return self.frame
 
-    def get_frame(self) -> BoxLayout:
+    def get_frame(self) -> GridLayout:
         if not self.frame:
             raise ValueError("Action Bar frame has not been built yet.")
         return self.frame
