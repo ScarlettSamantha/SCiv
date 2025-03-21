093c8f424df4edd99a2ae52123ea67ca458bb689
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-21 07:04:25 +0100

    🧑‍💻 ✨ Enhance save_inspect function to support direct path for saving data
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- save_inspect.py
+++ save_inspect.py
@@ -4,12 +4,15 @@ from pprint import pprint
 
 import dill
 
-uuid_path = "saves/{uuid}/data.pickle.gz"
+uuid_path = "saves/{uuid}/data.pickle.gz"  # This might be merged with direct_path but they are separate for now as they might be used differently.
+direct_path = "saves/{name}/data.pickle.gz"
 
 
 def save_inspect(path):
     if len(path) == 32 and "/" not in path:
         path = uuid_path.format(uuid=path)
+    elif "/" not in path:
+        path = direct_path.format(name=path)
 
     if path.endswith(".gz"):
         with gzip.open(path, "rb") as f:
