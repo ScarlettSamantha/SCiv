61c4328d8d0c26e5a696a340597446aff43117cd
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-26 10:23:46 +0100

    ✨ Add color conversion methods to Colors class
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- helpers/colors.py
+++ helpers/colors.py
@@ -1,3 +1,4 @@
+import typing
 from random import Random
 from typing import List, Tuple
 
@@ -46,3 +47,34 @@ class Colors:
         color = cls.COLORS[cls._sequence_index]
         cls._sequence_index += 1
         return color
+
+    @staticmethod
+    def to_hex(color: Tuple3f | Tuple4f, strip_alpha: bool = True) -> str:
+        if len(color) == 4:
+            if strip_alpha:
+                color = color[:3]
+            else:
+                return "#{:02x}{:02x}{:02x}{:02x}".format(
+                    int(color[0] * 255), int(color[1] * 255), int(color[2] * 255), int(color[3] * 255)
+                )
+        return "#{:02x}{:02x}{:02x}".format(int(color[0] * 255), int(color[1] * 255), int(color[2] * 255))
+
+    @staticmethod
+    def to_rgba(color: Tuple4f) -> str:
+        return "rgba({}, {}, {}, {})".format(int(color[0] * 255), int(color[1] * 255), int(color[2] * 255), color[3])
+
+    @staticmethod
+    def to_rgb(color: Tuple4f) -> str:
+        return "rgb({}, {}, {})".format(int(color[0] * 255), int(color[1] * 255), int(color[2] * 255))
+
+    @staticmethod
+    def from_hex(hex_color: str) -> Tuple3f:
+        hex_color = hex_color.lstrip("#")
+        rgb = tuple(int(hex_color[i : i + 2], 16) / 255 for i in (0, 2, 4))
+        return typing.cast(Tuple3f, rgb)
+
+    @staticmethod
+    def from_rgba(rgba_color: str) -> Tuple4f:
+        _rgba_color: list[str] = rgba_color.lstrip("rgba(").rstrip(")").split(",")
+        rgba = tuple(int(_rgba_color[i]) / 255 for i in (0, 1, 2, 3))
+        return typing.cast(Tuple4f, rgba)
