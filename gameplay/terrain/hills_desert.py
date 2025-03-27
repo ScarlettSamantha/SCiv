947538b2a98ed68e5146a8ddb0474cbc2279322f
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-27 07:13:26 +0100

    ♻️ Remove typo in radiation attribute from HillsDesert class
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- gameplay/terrain/hills_desert.py
+++ gameplay/terrain/hills_desert.py
@@ -7,7 +7,6 @@ class HillsDesert(BaseTerrain):
     _name = "world.terrain.hills_desert"
     movement_modifier = 0.5
     water_availability = 0.25
-    radatiation = 1.0  # Note: "radatiation" is used as in the original code.
     _model = "assets/models/tiles/hills_desert.glb"
     _texture = "assets/models/tiles/grass2.png"
 
