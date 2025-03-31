5236f50df0f1c5d3be181995204a8f0221b1f7c6
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-31 08:09:24 +0200

    🏷️  Update type hints for unit methods to use forward references
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- gameplay/_units.py
+++ gameplay/_units.py
@@ -11,11 +11,11 @@ class Units:
         self._units: List[UnitBaseClass] = []
         self._num_units: int = 0
 
-    def add_unit(self, unit: UnitBaseClass) -> None:
+    def add_unit(self, unit: "UnitBaseClass") -> None:
         self._units.append(unit)
         self._num_units += 1
 
-    def remove_unit(self, unit: UnitBaseClass):
+    def remove_unit(self, unit: "UnitBaseClass"):
         if unit in self._units:
             self._units.remove(unit)
             self._num_units -= 1
@@ -23,14 +23,14 @@ class Units:
     def __len__(self) -> int:
         return self._num_units
 
-    def all(self) -> List[UnitBaseClass]:
+    def all(self) -> List["UnitBaseClass"]:
         return self._units
 
-    def has(self, unit: UnitBaseClass) -> bool:
+    def has(self, unit: "UnitBaseClass") -> bool:
         return unit in self._units
 
     def has_any(self) -> bool:
         return len(self._units) > 0
 
-    def __iter__(self) -> Iterator[UnitBaseClass]:
+    def __iter__(self) -> Iterator["UnitBaseClass"]:
         return iter(self._units)
