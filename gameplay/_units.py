5d145b27827e8e7eb6710022faee367cb326a3f6
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-31 07:40:08 +0200

    🏷️  Remove generic type from Units class and update methods to use UnitBaseClass directly
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- gameplay/_units.py
+++ gameplay/_units.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Generic, Iterator, List, TypeVar
+from typing import TYPE_CHECKING, Iterator, List, TypeVar
 
 if TYPE_CHECKING:
     from gameplay.units.unit_base import UnitBaseClass
@@ -6,16 +6,16 @@ if TYPE_CHECKING:
 T = TypeVar("T", bound="UnitBaseClass")
 
 
-class Units(Generic[T]):
+class Units:
     def __init__(self):
-        self._units: List[T] = []
+        self._units: List[UnitBaseClass] = []
         self._num_units: int = 0
 
-    def add_unit(self, unit: T) -> None:
+    def add_unit(self, unit: UnitBaseClass) -> None:
         self._units.append(unit)
         self._num_units += 1
 
-    def remove_unit(self, unit: T):
+    def remove_unit(self, unit: UnitBaseClass):
         if unit in self._units:
             self._units.remove(unit)
             self._num_units -= 1
@@ -23,14 +23,14 @@ class Units(Generic[T]):
     def __len__(self) -> int:
         return self._num_units
 
-    def all(self) -> List[T]:
+    def all(self) -> List[UnitBaseClass]:
         return self._units
 
-    def has(self, unit: T) -> bool:
+    def has(self, unit: UnitBaseClass) -> bool:
         return unit in self._units
 
     def has_any(self) -> bool:
         return len(self._units) > 0
 
-    def __iter__(self) -> Iterator[T]:
+    def __iter__(self) -> Iterator[UnitBaseClass]:
         return iter(self._units)
