51a8aebfaee5f1977758700e2121119395eb295f
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-06-04 10:46:22 +0200

    🏷️ Add some type hint ignores in the shaders as they have not been annotated yet in panda.
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- system/shaders.py
+++ system/shaders.py
@@ -1,16 +1,16 @@
 from typing import Dict
 
-from panda3d.core import Shader
+from panda3d.core import Shader  # type: ignore
 
 
 class Shaders:
     def __init__(self):
         self.shaders: Dict[str, Shader] = {}
 
-    def load_shader(self, name: str, vert_path: str, frag_path: str) -> Shader:
+    def load_shader(self, name: str, vert_path: str, frag_path: str) -> Shader:  # type: ignore
         shader = Shader.load(Shader.SL_GLSL, vert_path, frag_path)  # type: ignore
-        self.shaders[name] = shader
+        self.shaders[name] = shader  # type: ignore
         return shader  # type: ignore
 
-    def get_shader(self, name: str) -> Shader | None:
-        return self.shaders.get(name)
+    def get_shader(self, name: str) -> Shader | None:  # type: ignore
+        return self.shaders.get(name)  # type: ignore
