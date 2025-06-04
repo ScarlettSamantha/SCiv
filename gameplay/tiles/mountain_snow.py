77d45c8eb39af13cb2c93693d3d52e0d54933f6f
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-06-04 10:50:32 +0200

    ♻️  Remove unused model attribute from Mountain and MountainSnow classes
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- gameplay/tiles/mountain_snow.py
+++ gameplay/tiles/mountain_snow.py
@@ -2,12 +2,10 @@ from typing import Any, Type
 
 from gameplay.terrain.mountain_snow import MountainSnow as MountainSnowTerrain
 from gameplay.tiles.base_tile import BaseTile
-from managers.i18n import T_TranslationOrStr
 
 
 class MountainSnow(BaseTile):
     _terrain: Type[MountainSnowTerrain] = MountainSnowTerrain
-    _model: T_TranslationOrStr = _terrain.get_model()
     _cache_name = "MountainSnow"
 
     def __init__(self, *args: Any, **kwargs: Any):
