e8f031807be7b95bedf77cc9667fb31c0b62347e
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-06-09 21:54:54 +0200

    🔧 Update icon path for Settler class to use relative path
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- gameplay/units/core/classes/civilian/settler.py
+++ gameplay/units/core/classes/civilian/settler.py
@@ -66,7 +66,7 @@ class Settler(CoreCivilianBaseClass):
     key = "core.unit.class.settler"
     name = t_("content.units.core.units.civilian.settler.name")
     description = t_("content.units.core.units.civilian.settler.description")
-    icon = "assets/icons/settler.png"
+    icon = "settler.png"
     promotion_tree = SettlerPromotionTree
     model_size = 0.5
 
