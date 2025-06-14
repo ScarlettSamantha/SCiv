65d8118bf8de5ea7ee96457f5c4a8324f400176b
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-06-14 20:11:19 +0200

    ♻️  Simplify model definition logic in TileRenderer by removing unnecessary coast check
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- system/tile_render.py
+++ system/tile_render.py
@@ -194,9 +194,7 @@ class TileRenderer:
 
         resource = res_list[0]
 
-        model_def = (
-            resource.get_land_model() if self.tile.is_land and not self.tile.is_coast else resource.get_water_model()
-        )
+        model_def = resource.get_land_model() if self.tile.is_land else resource.get_water_model()
 
         if model_def:
             self.add_model(
