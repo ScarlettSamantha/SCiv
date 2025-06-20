3c183d69d7566e9139826cfb632cc29e94be0fe1
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-06-20 12:10:21 +0200

    🔧 Fix program path in launch configuration and update import statement in __main__.py
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- sciv/__main__.py
+++ sciv/__main__.py
@@ -1,5 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, SCRIPT_DIR)
+os.chdir(SCRIPT_DIR)
+
+
 def bootstrap():
-    from .game import OpenCiv
+    from game import OpenCiv
 
     app = OpenCiv(debug=True)
 
