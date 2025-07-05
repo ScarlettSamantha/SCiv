04388b3909ecaf1f6e07bc6c29287cc3c0393291
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-07-05 07:10:39 +0200

    ♻️ Refactor Core AI with Enhancements to the type hints throughout AI implementation.
    Also standardizes parameter naming, and improves serialization logic of the AI core implementation.
    
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- sciv/gameplay/ai/core.py
+++ sciv/gameplay/ai/core.py
@@ -13,18 +13,15 @@ from gameplay.cities import Cities
 from gameplay.personality import Personality
 from gameplay.player_tiles import PlayerTiles
 from gameplay.repositories.tile import TileRepository
-from gameplay.tile import Tile
-from gameplay.unit import Unit
 from helpers.cache import Optional
 from managers.game import World
 from managers.player import PlayerManager
 from managers.turn import Turn
 
-from sciv.gameplay.player import Player
-
 if TYPE_CHECKING:
     from gameplay.player import Player
     from gameplay.tile import Tile
+    from gameplay.unit import Unit
     from gameplay.vision import Vision
 
 
@@ -34,11 +31,11 @@ class AI(ABC):
     def __init__(self, player: "Player"):
         self._player: weakref.ReferenceType["Player"] = weakref.ref(player)
         self.turn_action_register: Dict[int, List[Callable[..., None]]] = {}
-        self.logger: Logger = self.player.logger.getChild("ai")
+        self.logger: Logger = self.player.logger.getChild(suffix="ai")
         self.logger.debug(f"AI created for player {str(self.player.name)} with personality {self.player.personality}")
 
-        self.control_units: weakref.ReferenceType[Units] = weakref.ref(self.player.units)
-        self.control_cities: weakref.ReferenceType[Cities] = weakref.ref(self.player.cities)
+        self.control_units: weakref.ReferenceType["Units"] = weakref.ref(self.player.units)
+        self.control_cities: weakref.ReferenceType["Cities"] = weakref.ref(self.player.cities)
         self.control_tiles: weakref.ReferenceType[PlayerTiles] = weakref.ref(self.player.tiles)
         self.vision: weakref.ReferenceType["Vision"] = weakref.ref(self.player.vision)
         self.unit_directions: Dict[int, Tuple[int, int]] = {}
@@ -58,31 +55,30 @@ class AI(ABC):
         self.vision = weakref.ref(self.player.vision)
 
     def __getstate__(self) -> Dict[str, Any]:
-        data = self.__dict__.copy()
+        data: Dict[str, Any] = self.__dict__.copy()
+        # These can all be recreated from the player reference
+
+        player: "Player | None" = self._player()
+        if player is None:
+            raise ValueError("Player reference is None, cannot serialize AI state.")
+
         data.pop("control_units", None)
+        data.pop("control_cities", None)
+        data.pop("control_tiles", None)
         data.pop("logger", None)
         data.pop("vision", None)
+        data["player"] = player.get_tag()
         return data
-        # return {
-        #     "turn_action_register": self.turn_action_register,
-        #     "player": self._player,
-        #     "memory": self.memory,
-        #     "tasks": self.tasks,
-        #     "personality": self.personality,
-        #     "end_goal": self.end_goal,
-        #     "goals": self.goals,
-        #     "unit_directions": self.unit_directions,
-        # }
 
     def __setstate__(self, state: Dict[str, Any]) -> None:
         self.__dict__.update(state)
-        player_ref: Player | None = self._player()
+        player_ref: "Player | None" = self._player()
         if player_ref is not None:
             self.logger = player_ref.logger.getChild("ai")
 
     @property
     def player(self) -> "Player":
-        player = self._player()
+        player: "Player | None" = self._player()
         if player is None:
             self._player = weakref.ref(PlayerManager.get_nature())
             assert self._player() is not None, "Player reference is None"
@@ -98,7 +94,7 @@ class AI(ABC):
     def get_units(self) -> Units:
         if self.control_units() is None:
             self.control_units = weakref.ref(self.player.units)
-        units = self.control_units()
+        units: "Units | None" = self.control_units()
         if units is None:
             raise ValueError("Units reference is None")
         return units
@@ -122,31 +118,31 @@ class AI(ABC):
         return self.personality
 
     def add_memory(self, memory: Memory) -> None:
-        self.memory.add_memory(memory)
+        self.memory.add_memory(memory=memory)
 
     def add_task(self, task: Task) -> None:
-        self.tasks.add_task(task)
+        self.tasks.add_task(task=task)
 
     def add_goal(self, goal: Goal) -> None:
-        self.goals.add_goal(goal)
+        self.goals.add_goal(goal=goal)
 
     def add_end_goal(self, goal: Goal) -> None:
-        self.end_goal.add_goal(goal)
+        self.end_goal.add_goal(goal=goal)
 
     def has_goal(self) -> bool:
         return len(self.goals) > 0
 
     def remove_end_goal(self, goal: Goal) -> None:
-        self.end_goal.remove_goal(goal)
+        self.end_goal.remove_goal(goal=goal)
 
     def remove_goal(self, goal: Goal) -> None:
-        self.goals.remove_goal(goal)
+        self.goals.remove_goal(goal=goal)
 
     def remove_memory(self, memory: Memory) -> None:
-        self.memory.remove_memory(memory)
+        self.memory.remove_memory(memory=memory)
 
     def remove_task(self, task: Task) -> None:
-        self.tasks.remove_task(task)
+        self.tasks.remove_task(task=task)
 
     @abstractmethod
     def register_end_goal(self) -> Goals: ...
@@ -162,7 +158,7 @@ class AI(ABC):
 
     def on_turn_end(self) -> None:
         turn: int = Turn.get_singleton_instance().get_turn()
-        self.execute_turn_action(turn)
+        self.execute_turn_action(turn=turn)
 
     @abstractmethod
     def on_turn_start(self) -> None: ...
@@ -170,61 +166,49 @@ class AI(ABC):
     @abstractmethod
     def on_game_start(self) -> None: ...
 
-    def spawn_unit(self, unit: Type["Unit"], tile: Tile) -> "Unit":
-        """
-        Spawn a unit on the given tile.
-        """
-        return unit.spawn_on(tile, self.get_player())
+    def spawn_unit(self, unit: Type["Unit"], tile: "Tile") -> "Unit":
+        return unit.spawn_on(tile=tile, player=self.get_player())
 
-    def get_world_state(self) -> Dict[Tuple[int, int], Tile]:
-        """
-        Get the world state.
-        """
+    def get_world_state(self) -> Dict[Tuple[int, int], "Tile"]:
         return self.world()
 
-    def world(self) -> Dict[Tuple[int, int], Tile]:
+    def world(self) -> Dict[Tuple[int, int], "Tile"]:
         return World.get_singleton_instance().get_grid()
 
     def get_tile_count(self) -> int:
         return len(self.world())
 
     def get_players(self) -> Dict[int, "Player"]:
-        """
-        Get all players in the game.
-        """
         return PlayerManager.all()
 
-    def get_targets(self) -> Dict[Tuple[int, int], Tile]:
-        targets: Dict[Tuple[int, int], Tile] = {}
+    def get_targets(self) -> Dict[Tuple[int, int], "Tile"]:
+        targets: Dict[Tuple[int, int], "Tile"] = {}
         for unit in self.get_units().all():
             targets.update(self.get_target_for_unit(unit))
         return targets
 
-    def create_goals_for_unit(self, executing_unit: Unit) -> Optional[Goal]:
-        targets = self.get_target_for_unit(executing_unit)
+    def create_goals_for_unit(self, executing_unit: "Unit") -> Optional[Goal]:
+        targets: Dict[Tuple[int, int], "Tile"] = self.get_target_for_unit(executing_unit)
         if len(targets) == 0:
             return None
 
         for tile in targets.values():
             if tile.units.has_any():
-                target_unit: Unit | None = tile.units.first()
+                target_unit: "Unit | None" = tile.units.first()
                 if target_unit is None or target_unit.owner == self.get_player():
                     continue
                 return EliminateUnit(target=target_unit, executing_unit=executing_unit, parent=weakref.ref(self))
 
-    def get_goals_for_unit(self, executing_unit: Unit) -> List[Goal]:
-        """
-        Get the goals for a specific unit.
-        """
+    def get_goals_for_unit(self, executing_unit: "Unit") -> List[Goal]:
         goals: List[Goal] = []
         for unit_goal in self.get_goals():
             if unit_goal.for_unit and unit_goal.get_executing_unit() == executing_unit:
                 goals.append(unit_goal)
         return goals
 
-    def get_target_for_unit(self, unit: Unit) -> Dict[Tuple[int, int], Tile]:
-        targets: Dict[Tuple[int, int], Tile] = {}
-        for tile in unit.look(self.UNIT_REAL_VISION_RADIUS):
+    def get_target_for_unit(self, unit: "Unit") -> Dict[Tuple[int, int], "Tile"]:
+        targets: Dict[Tuple[int, int], "Tile"] = {}
+        for tile in unit.look(radius=self.UNIT_REAL_VISION_RADIUS):
             if tile.is_city() and tile.owner != self.get_player():
                 targets[tile.x, tile.y] = tile
             elif tile.units.has_any() and self.is_target(tile.units.first()):  # type: ignore
@@ -233,14 +217,14 @@ class AI(ABC):
                 targets[tile.x, tile.y] = tile
         return targets
 
-    def get_threats(self) -> Dict[Tuple[int, int], Tile]:
+    def get_threats(self) -> Dict[Tuple[int, int], "Tile"]:
         threats: Dict[Tuple[int, int], Tile] = {}
         for unit in self.get_units().all():
             threats.update(self.get_threat_for_unit(unit))
         return threats
 
-    def get_threat_for_unit(self, unit: Unit) -> Dict[Tuple[int, int], Tile]:
-        threats: Dict[Tuple[int, int], Tile] = {}
+    def get_threat_for_unit(self, unit: "Unit") -> Dict[Tuple[int, int], "Tile"]:
+        threats: Dict[Tuple[int, int], "Tile"] = {}
         for tile in unit.look(self.UNIT_REAL_VISION_RADIUS):
             if tile.is_city() and tile.owner != self.get_player():
                 threats[tile.x, tile.y] = tile
@@ -248,37 +232,28 @@ class AI(ABC):
                 threats[tile.x, tile.y] = tile
         return threats
 
-    def is_target(self, unit: Unit) -> bool:
-        """
-        Check if the unit is a target.
-        """
+    def is_target(self, unit: "Unit") -> bool:
         result = unit.owner != self.get_player()
         return result
 
-    def is_threat(self, unit: Unit) -> bool:
-        """
-        Check if the unit is a threat.
-        """
+    def is_threat(self, unit: "Unit") -> bool:
         result = unit.owner != self.get_player()
         return result
 
     def check_route_to(self, _from: "Tile", to: "Tile", radius: int = 5) -> bool:
-        return TileRepository.astar(_from, to, radius) is not None
+        return TileRepository.astar(start=_from, goal=to, movement_points=radius) is not None
 
-    def move_unit(self, unit: Unit, to: Tile, on_tile_visit: Optional[Callable[["Tile"], None]] = None) -> None:
-        """
-        Move a unit to a specific tile.
-        """
-        movement_points = unit.moves_left
-        if (path := TileRepository.astar(unit.get_tile(), to, movement_points)) is not None:
+    def move_unit(self, unit: "Unit", to: "Tile", on_tile_visit: Optional[Callable[["Tile"], None]] = None) -> None:
+        movement_points: int | float = unit.moves_left
+        if (path := TileRepository.astar(start=unit.get_tile(), goal=to, movement_points=movement_points)) is not None:
             for path_tile in path:
-                unit.move(path_tile)
+                unit.move(tile=path_tile)
                 if on_tile_visit is not None:
                     on_tile_visit(path_tile)
 
     def _select_wander_tile(self, unit: "Unit") -> Optional["Tile"]:
-        current = unit.get_tile()
-        neighbors = [
+        current: "Tile" = unit.get_tile()
+        neighbors: List["Tile"] = [
             t
             for t in TileRepository.get_neighbors(current, 1, True, False)
             if t.is_passable() and not t.units.has_any() and not t.is_lake and not t.is_water
@@ -286,15 +261,13 @@ class AI(ABC):
         if not neighbors:
             return None
 
-        uid = id(unit)
-        prev_dir = self.unit_directions.get(uid, None)
+        uid: int = id(unit)
+        prev_dir: Tuple[int, int] | None = self.unit_directions.get(uid, None)
 
         if prev_dir:
-            scored: List[Tuple[int, Tile]] = []
+            scored: List[Tuple[int, "Tile"]] = []
             for t in neighbors:
-                dx = t.x - current.x
-                dy = t.y - current.y
-                score = dx * prev_dir[0] + dy * prev_dir[1]
+                score: int = (t.x - current.x) * prev_dir[0] + (t.y - current.y) * prev_dir[1]
                 scored.append((score, t))
 
             best_score, best_tile = max(scored, key=lambda pair: pair[0])
@@ -305,26 +278,20 @@ class AI(ABC):
 
     def on_wander(self, unit: "Unit") -> None:
         while unit.moves_left > 0:
-            current = unit.get_tile()
-            next_tile = self._select_wander_tile(unit)
+            current: Tile = unit.get_tile()
+            next_tile: Tile | None = self._select_wander_tile(unit)
             if not next_tile:
                 break
 
-            dx = next_tile.x - current.x
-            dy = next_tile.y - current.y
-            self.unit_directions[id(unit)] = (dx, dy)
+            self.unit_directions[id(unit)] = ((next_tile.x - current.x), (next_tile.y - current.y))
+            self.move_unit(unit=unit, to=next_tile)
 
-            self.move_unit(unit, next_tile)
-
-            new_goal = self.create_goals_for_unit(unit)
+            new_goal: Goal | None = self.create_goals_for_unit(executing_unit=unit)
             if new_goal:
-                self.add_goal(new_goal)
+                self.add_goal(goal=new_goal)
                 return
 
     def on_fixed_turn(self, turn: int, callable: Callable[[], None]) -> None:
-        """
-        Register a callable to be executed on a specific turn.
-        """
         if turn in self.turn_action_register:
             raise ValueError(f"Turn {turn} already has an action registered.")
         self.turn_action_register.setdefault(turn, []).append(callable)
