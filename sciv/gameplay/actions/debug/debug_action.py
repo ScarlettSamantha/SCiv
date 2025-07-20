c4b2a97e3eba17f42c7d08be50734e63771bd28a
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-07-20 12:46:17 +0200

    ✨ Actions can now more easily be injected into the game ui to wait for a targeting click.
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

---
+++ sciv/gameplay/actions/debug/debug_action.py
@@ -0,0 +1,25 @@
+from typing import Any, Callable, List, Optional, Self
+
+from managers.i18n import T_TranslationOrStr
+from system.actions import Action
+
+
+class DebugAction(Action):
+    key = "action.debug.base"
+    debug_action = True
+
+    def __init__(
+        self,
+        action: Optional[Callable[[Self, Any, Any], Optional[Any]]] = None,
+        name: T_TranslationOrStr = key,
+        *args: List[Any],
+        **kwargs: Any,
+    ):
+        if action is None:
+            action = self.undefined_action
+        super().__init__(name=name, action=action, *args, **kwargs)
+
+    def undefined_action(
+        self, *args: Any, **kwargs: Any
+    ) -> None:  # This is done for syntax sugar as otherwise mypy would complain but child classes should implement this method or override the reference to this function.
+        raise NotImplementedError("DebugAction must implement run_action method.")
--- sciv/menus/kivy/parts/debug_actions.py
+++ sciv/menus/kivy/parts/debug_actions.py
@@ -1,181 +1,83 @@
-from enum import Enum
-from logging import Logger
-from typing import TYPE_CHECKING, Any, Optional
+from typing import TYPE_CHECKING, Any, Dict
 
-from direct.showbase.MessengerGlobal import messenger
+from gameplay.actions.debug.debug_action import DebugAction
+from kivy.graphics import Color, Rectangle
+from kivy.metrics import dp
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.uix.spinner import Spinner
-
-from helpers.colors import Tuple4f
-from menus.kivy.elements.button_value import ButtonValue
-from menus.kivy.mixins.collidable import CollisionPreventionMixin
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+from managers.debug import DebugManager
 
 if TYPE_CHECKING:
-    from game import OpenCiv
+    from menus.screens.game_ui import GameUIScreen
 
 
-class MapActionsValues(Enum):
-    NONE = "None"
-    SMALL_BIG_ICONS = "Small+Big Icons"
-    SMALL_ICONS = "Small Icons"
-    BIG_ICONS = "Big Icons"
-    ALL_IN_RADIUS = "All in Radius"
+class DebugActions:
+    def __init__(self, screen: "GameUIScreen"):
+        self.screen: "GameUIScreen" = screen
+        self.is_open: bool = False
 
-
-class LenseOptionsValues(Enum):
-    NONE = "None"
-    UNITS = "Units"
-    WATER = "Water"
-    RESOURCES = "Resources"
-    EMPIRES = "Empires"
-
-
-class DebugUIOptionsValues(Enum):
-    NONE = "None"
-    DEBUG = "Debug"
-    STATS = "Stats"
-    ACTIONS = "Actions"
-    DEBUG_AND_STATS = "Debug+Stats"
-    DEBUG_AND_ACTIONS = "Debug+Actions"
-    ALL_DEBUG_UI = "All Debug UI"
-
-
-class DebugActions(
-    BoxLayout,
-    CollisionPreventionMixin,
-):
-    def __init__(
-        self,
-        base: "OpenCiv",
-        logger: Logger,
-        background_color: Tuple4f = (0, 0, 0, 0),
-        border: Tuple4f = (0, 0, 0, 0),
-        **kwargs: Any,
-    ):
-        self.background_color = background_color
-        self.border = border
-        self.background_image = None
-        super(BoxLayout).__init__(**kwargs)  # type: ignore
-        super(CollisionPreventionMixin, self).__init__(**kwargs)
-        self.base = base
-        self.frame: Optional[BoxLayout] = None
-        self.logger: Logger = logger.getChild("screen.parts.debug_actions")
-
-        self.toggle_big_icons: Optional[Button] = None
-        self.lenses_dropdown: Optional[Spinner] = None
-        self.map_actions: Optional[Spinner] = None
-        self.debug_ui_spinner: Optional[Spinner] = None
-        self.lenses_dropdown_open: bool = False
-
-    def build(self) -> BoxLayout:
-        # --- Action Bar (Bottom Centered) ---
-        self.frame = BoxLayout(
-            orientation="horizontal",
-            size_hint=(None, None),
-            width=1000,
-            height=200,
-            spacing=10,
-            pos_hint={"center_x": 0.5, "y": 0.92},
+        self.frame: BoxLayout = BoxLayout(
+            orientation="vertical", size_hint=(0.9, 0.9), pos_hint={"center_x": 0.5, "center_y": 0.5}
         )
 
-        self.lenses_dropdown = Spinner(
-            text="Lenses(None)",
-            size_hint=(None, None),
-            width=150,
-            height=80,
+        self.debug_manager: DebugManager = DebugManager.get_singleton_instance()
+
+        with self.frame.canvas.before:
+            Color(0.7, 0.7, 0.7, 1)
+            self._bg_rect = Rectangle(pos=self.frame.pos, size=self.frame.size)
+        self.frame.bind(pos=self._update_bg, size=self._update_bg)
+
+        self.header: Widget = Widget(size_hint_y=None, height=dp(30))
+        with self.header.canvas.before:  # type: ignore
+            Color(0, 0, 0, 1)
+            self._header_rect = Rectangle(pos=self.header.pos, size=self.header.size)  # type: ignore
+        self.header.bind(pos=self._update_header, size=self._update_header)
+
+        self.content: GridLayout = GridLayout(orientation="lr-tb", cols=3, spacing=dp(5), size_hint_y=None)
+        self.buttons: Dict[str, Widget] = {}
+
+        self.frame.add_widget(self.header)
+        self.frame.add_widget(self.content)
+
+    def build_buttons(self) -> None:
+        actions: Dict[str, DebugAction] = self.debug_manager.get_all_debug_actions()
+        for _, action in actions.items():
+            btn: Widget = self._create_button(action)
+            assert isinstance(btn, Widget), "Button creation failed"
+            self.buttons[action.key] = btn
+            self.content.add_widget(btn)
+
+    def _create_button(self, action: DebugAction) -> Widget:
+        btn = Button(
+            text=action.key,
+            size_hint_y=None,
+            height=dp(40),
         )
+        assert isinstance(btn, Button), "Button creation failed"
+        btn.size_hint_y = None
+        btn.height = dp(40)
+        return btn
 
-        if self.lenses_dropdown._dropdown is None:  # type: ignore
-            raise ValueError("Dropdown is not initialized.")
+    def _update_bg(self, instance: Widget, value: Any) -> None:
+        self._bg_rect.pos = instance.pos  # type: ignore
+        self._bg_rect.size = instance.size  # type: ignore
 
-        self.lenses_dropdown._dropdown.bind(on_select=self.on_lense_change_request)  # type: ignore
-        self.lenses_dropdown.values = [str(v.value) for _, v in LenseOptionsValues.__members__.items()]
-        self.frame.add_widget(self.lenses_dropdown)
+    def _update_header(self, instance: Widget, value: Any) -> None:
+        self._header_rect.pos = instance.pos  # type: ignore
+        self._header_rect.size = instance.size  # type: ignore
 
-        self.map_actions = Spinner(
-            text="Resources (Big Only)",
-            size_hint=(None, None),
-            width=150,
-            height=80,
-        )
+    def show(self) -> None:
+        if not self.buttons:
+            self.build_buttons()
 
-        if self.map_actions._dropdown is None:  # type: ignore
-            raise ValueError("Dropdown is not initialized.")
+        self.frame.disabled = False
+        self.frame.opacity = 1
+        self.is_open = True
 
-        self.map_actions._dropdown.bind(on_select=self.on_resource_ui_change_request)  # type: ignore
-        self.map_actions.values = [str(v.value) for _, v in MapActionsValues.__members__.items()]
-        self.frame.add_widget(self.map_actions)
-
-        items = {str(v.value): k for k, v in DebugUIOptionsValues.__members__.items()}
-        self.debug_ui_spinner = Spinner(
-            text="Debug UI (ALL)",
-            size_hint=(None, None),
-            width=150,
-            height=80,
-            on_select=self.on_debug_ui_change_request,
-            values=items,
-        )
-
-        self.register_non_collidable(self.debug_ui_spinner)
-
-        if self.debug_ui_spinner._dropdown is None:  # type: ignore
-            raise ValueError("Dropdown is not initialized.")
-
-        elif self.debug_ui_spinner._dropdown.container is not None:  # type: ignore
-            for child in self.debug_ui_spinner._dropdown.container.children:  # type: ignore
-                self.register_non_collidable(child)  # type: ignore
-            self.debug_ui_spinner._dropdown.bind(on_select=self.on_debug_ui_change_request)  # type: ignore
-            self.frame.add_widget(self.debug_ui_spinner)
-
-        return self.frame
-
-    def on_lense_change_request(self, instance: Button, selected_text: str):
-        # 'instance.text' will now be one of "None", "Units", "Water", "Resources"
-        state_enum = next((_member for _member in LenseOptionsValues if _member.value == selected_text), None)
-        if state_enum is None:
-            self.logger.error(f"No matching state for {instance}")
-            return
-
-        self.logger.info(f"Lense change requested to state {str(state_enum.value)}.")
-        messenger.send("ui.update.ui.lense_change", [state_enum])
-
-    def on_resource_ui_change_request(self, instance: Button, selected_text: str):
-        state_enum = next(
-            (_member for _member in MapActionsValues if _member.value.lower() == selected_text.lower()), None
-        )
-
-        if state_enum is None:
-            self.logger.error(f"No matching state for {instance.text}")
-            return
-
-        self.logger.info(f"Resource UI change requested to state {str(state_enum.value)}.")
-        messenger.send("ui.update.ui.resource_ui_change", [state_enum])
-
-    def on_debug_ui_change_request(self, instance: ButtonValue, selected_text: str):
-        state_enum = next(
-            (_member for _member in DebugUIOptionsValues if _member.value.lower() == selected_text.lower()), None
-        )
-        if state_enum is None:
-            self.logger.error(f"No matching state for {instance.text}")
-            return
-
-        self.logger.info(f"Debug UI change requested to state {str(state_enum.name)}.")
-        messenger.send("ui.update.ui.debug_ui_toggle", [state_enum])
-
-    def get_frame(self) -> BoxLayout:
-        if not self.frame:
-            raise ValueError("Action Bar frame has not been built yet.")
-        return self.frame
-
-    def add_button(self, button: Button):
-        self.get_frame().add_widget(button)
-        return button
-
-    def remove_button(self, button: Button):
-        self.get_frame().remove_widget(button)
-        return button
-
-    def clear_buttons(self):
-        self.get_frame().clear_widgets()
-        return self.frame
+    def hide(self) -> None:
+        self.frame.disabled = True
+        self.frame.opacity = 0
+        self.is_open = False
+        self.current_entity = None
