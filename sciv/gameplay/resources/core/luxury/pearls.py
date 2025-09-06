1d650d0cf643cf4330ea7849235bcf948b1e4b61
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-09-06 08:14:25 +0200

    ♻️ Update spawn chance for pearls resource
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- sciv/gameplay/resources/core/luxury/pearls.py
+++ sciv/gameplay/resources/core/luxury/pearls.py
@@ -4,10 +4,10 @@ from gameplay.improvements.core.resources.fishing_boats import FishingBoats
 from gameplay.resource import ResourceSpawnablePlace
 from gameplay.resources.core.luxury.luxury_resource import BaseLuxuryResource
 from gameplay.terrain._base_terrain import BaseTerrain
-from managers.i18n import T_TranslationOrStr, t_
-
 from gameplay.terrain.coast import Coast
+from gameplay.terrain.lake import Lake
 from gameplay.terrain.sea import Sea
+from managers.i18n import T_TranslationOrStr, t_
 
 
 class Pearls(BaseLuxuryResource):
@@ -16,7 +16,7 @@ class Pearls(BaseLuxuryResource):
     description: T_TranslationOrStr = t_("content.resources.core.pearls.description")
     _color = (1.0, 1.0, 0.0)
     icon: str = "assets/icons/resources/core/luxury/bordered_pearls.png"
-    spawn_chance: float | Dict[Type[BaseTerrain], float] = {BaseTerrain: 5.0, Sea: 20.0, Coast: 90.0}
+    spawn_chance: float | Dict[Type[BaseTerrain], float] = {BaseTerrain: 0.0, Sea: 20.0, Coast: 90.0, Lake: 50.0}
     coverage = 2.0
     spawn_type: ResourceSpawnablePlace = ResourceSpawnablePlace.WATER
     improvement_required = [FishingBoats]
